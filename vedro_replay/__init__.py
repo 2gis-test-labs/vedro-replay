4f942db4705ded1c83ff8585d0351c8a29b7787b
Konstantin Shefer <k.shefer@2gis.ru>
2022-12-08 14:16:21 +0700

    add Excluder in init

--- vedro_replay/__init__.py
+++ vedro_replay/__init__.py
@@ -1,7 +1,7 @@
 from .response import Response, JsonResponse, MultipartResponse
-from .excluder import filter_response
+from .excluder import filter_response, Excluder
 from .config import Config
 from .replay import replay
 from .generator import generation
 
-__ALL__ = (generation, filter_response, replay, Config, Response, JsonResponse, MultipartResponse)
+__ALL__ = (replay, generation, filter_response, Excluder, Config, Response, JsonResponse, MultipartResponse)
